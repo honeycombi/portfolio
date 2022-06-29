9742b321012c0430355142c04ce7916be0b2e59b
Gary Hu <83254658+GaryHu2024@users.noreply.github.com>
2022-06-28 22:15:30 -0400

    comments enable 2

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -55,11 +55,11 @@ const Giscus = () => {
   }, [commentsTheme])
 
   // Reload on theme change
-  useEffect(() => {
-    const iframe = document.querySelector('iframe.giscus-frame')
-    if (!iframe) return
-    LoadComments()
-  }, [LoadComments])
+  // useEffect(() => {
+  //   const iframe = document.querySelector('iframe.giscus-frame')
+  //   if (!iframe) return
+  //   LoadComments()
+  // }, [LoadComments])
 
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
